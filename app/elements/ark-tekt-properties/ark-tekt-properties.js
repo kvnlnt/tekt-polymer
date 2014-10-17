e62dfa458e903dd97d0384c6d655654118824d05
Kevin Lint <kevin@flowroute.com>
2014-10-17 08:37:05 -0700

    scaffold data-binding for properties

--- app/elements/ark-tekt-properties/ark-tekt-properties.js
+++ app/elements/ark-tekt-properties/ark-tekt-properties.js
@@ -5,7 +5,32 @@
     Polymer('ark-tekt-properties', {
 
         // publish properties
-        publish: {},
+        publish: {
+            endpoint:'/properties',
+            properties:null
+        },
+
+        // ajax properties
+        getProperties: function(){
+
+            var data = {
+                'properties':[
+                    {
+                        id:1,
+                        property:'kevinlint.com'
+                    },
+                    {
+                        id:2,
+                        property:'linttraprecords.com'
+                    }
+                ]
+            };
+
+            scope.properties.model = data;
+
+            return data;
+
+        },
 
         // Fires when an instance of the element is created
         created: function() { 
@@ -15,10 +40,18 @@
         },
 
         // Fires when the element’s initial set of children and siblings are guaranteed to exist
-        domReady: function() {},
+        domReady: function() {
+
+            scope.getProperties();
+
+        },
 
         // Fires when the "<polymer-element>" has been fully prepared
-        ready: function() {},
+        ready: function() {
+
+            scope.properties = this.shadowRoot.getElementById('properties');
+
+        },
 
         // Fires when the element was inserted into the document
         attached: function() {},
